fe0a4c128588a96ac06ff74787d5cc725a931a5b
MichaelNeely77 <michael@michaelneely.net>
2023-10-25 09:28:38 -0400

    Added the instructions for the Rock, Paper, Scissotrs game and starting to lay out the project

---
+++ script.js
@@ -0,0 +1,23 @@
+// Begin with function called getComputerChoice
+// Function will randomly return either 'Rock', 'Paper', or 'Scissors'.
+
+// function getComputerChoice() {
+//
+// }
+// Write a function that plays a single round of "Rock, Paper, Scissors".
+// The function should take two parameters, playerSelection and computerSelection
+//And then return a string that declares the winner of the round like so: "You Lose! Paper Beats Rock"
+// String will probably use template literals
+
+// function playRound(playerSelection, computerSelection) {
+//
+// }
+//
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+// Write a new function called game().
+//Add the previous code inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+// Use prompt() to get input from user
+
