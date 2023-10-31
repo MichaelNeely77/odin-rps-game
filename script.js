b7fc2fc5005f2d8e6085aea56912f45a1af4c347
MichaelNeely77 <michael@michaelneely.net>
2023-10-31 09:46:59 -0400

    Added functional logic for returning 'rock', 'paper', 'scissors' in console.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // Begin with function called getComputerChoice
 // Function will randomly return either 'Rock', 'Paper', or 'Scissors'.
 
-// function getComputerChoice() {
-//
-// }
+function getComputerChoice(min, max) {
+  let result = Math.floor(Math.random() * (max - min) + min);
+
+  if (result === 1) {
+      console.log('Rock');
+  } else if (result === 2) {
+      console.log('Paper');
+  } else {
+      console.log('Scissors');
+  }
+
+    // console.log(result);
+
+}
+console.log(getComputerChoice(1,4));
+
+
+
 // Write a function that plays a single round of "Rock, Paper, Scissors".
+
+
 // The function should take two parameters, playerSelection and computerSelection
 //And then return a string that declares the winner of the round like so: "You Lose! Paper Beats Rock"
 // String will probably use template literals
